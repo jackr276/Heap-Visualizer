9e2a0e76017d1ee64d4f587be030e11a2c0bba45
Jack Robbins <jack_m_robbins@me.com>
2024-06-13 19:16:48 -0400

    commit algorithm

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,21 @@ public class Main{
 		
 		MinHeap heap = new MinHeap(10);
 		
-		heap.insert(0).insert(1).insert(5).insert(7).insert(11).insert(10).insert(2);
+		heap.insert(8)
+			.insert(1)
+			.insert(5)
+			.insert(7)
+			.insert(11)
+			.insert(10)
+			.insert(2)
+			.insert(4)
+			.insert(0)
+			.insert(7)
+			.insert(9)
+			.insert(20)
+			.insert(15)
+			.insert(21)
+			.insert(-1);
 		heap.printAsBinaryTree();
 	}
 }
--- src/MinHeap.java
+++ src/MinHeap.java
@@ -52,25 +52,46 @@ public class MinHeap{
 		return this;
 	}
 
+	/**
+	 * As part of a heap class, print a nice visualization of a heap to the
+	 * console
+	 */
 	public void printAsBinaryTree(){
-		final int LINE_WIDTH = 70;
+		//For the user, print out the array to begin with
+		System.out.println("Heap: " + Arrays.toString(heap) + "\n");
+		//calculate a given line width	
+		final int WIDTH = this.size * 5;
+		//Define an array to hold everything that we are printing
 		int[] printing = new int[this.size];
+
+		//Current level we are on
 		int current_level = 0;
 		int x = 1;	
 
+		//Set the "root" effectively
 		printing[0] = this.heap[0];
+
+		//Go through the entire array
 		for(int i = 0, j = 1; i < size; i++, j++){
-			int position = (int)(printing[parent(i)] + ((i % 2) == 0 ? -1 : 1)
-							* (LINE_WIDTH * Math.pow(2, current_level + 1) + 1));
-			
+			//Calculate the appropriate number of '-' or ' ' to print to the console
+			//If we have an odd i, go back(-1) and if we have an even i go forward(1)
+			int position = (int)(printing[parent(i)] + ((i % 2) == 1 ? -1 : 1)
+							* (WIDTH / Math.pow(2, current_level + 1) + 1));
+		
+			//Print the appropriate number of '-' or ' '
 			for(int k = 0; k < position - x; k++){
-				System.out.printf("%c", (i==0 || i % 2 ==1) ? ' ' : '-');
+				//If we land on an odd or 0 index, print out spaces, dashes otherwise
+				System.out.printf("%c", (i == 0 || i % 2 == 1) ? ' ' : '-');
 			}
 
+			//Print the actual number out
 			System.out.printf("%d", heap[i]);
+			//Put i back in as the parent
 			printing[i] = x = position + 1;
 
+			//We can tell if we need to "drill down" a level
 			if(j == Math.pow(2, current_level)){
+				//Print a newline, go down a level, and reset our variables
 				System.out.println();
 				current_level++;
 				x = 1;
